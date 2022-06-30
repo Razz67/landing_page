f4b55030a747319c3da4d40b1ef423bda160c081
Stephine Rasberry <srasberry00@gmail.com>
2022-06-30 11:02:57 -0400

    Add Scroll Button

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -18,23 +18,6 @@
  * Great to have comments before crucial code sections within the procedure.
  */
 
-/**
- * Define Global Variables
- *
- */
-
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-
 // build the nav
 const menu = document.getElementById("navbar__list");
 const sections = [...document.querySelectorAll(".section")];
@@ -45,16 +28,53 @@ const nav_menu_items = () => {
   sections.forEach((section) => {
     const sectionID = section.id;
     const sectionAtrribute = section.dataset.nav;
-    nav_container += `<li> <a class="navbar__menu" href="#${sectionID}">${sectionAtrribute}</a></li>`;
+    nav_container += `<li> <a class="navbar__menu active" href="#${sectionID}">${sectionAtrribute}</a></li>`;
   });
   menu.innerHTML = nav_container;
 };
 nav_menu_items();
 
+// Add active class to nav bar buttons
+
+let active_btn = document.querySelectorAll(".navbar__list a");
+for (let i = 0, length = active_btn.length; i < length; i++) {
+    active_btn[i].onclick = function() {
+        const b = document.querySelector(".navbar__list li.active");
+        if (b) b.classList.remove("active");
+        this.parentNode.classList.add("active");
+    };
+}
+
 // Add class 'active' to section when near top of viewport
 
-// Scroll to anchor ID using scrollTO event
 
+
+// Scroll to anchor ID using scrollTO event
+var topBtn = document.getElementById("topBtn");
+// Show button when user scrolls down 50px
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+}
+
+// Scroll to top when button clicked
+function topFunction() {
+  //
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+
+//
 /**
  * End Main Functions
  * Begin Events

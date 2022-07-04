ade24f2a3c179812d659443bcff62c329f3e2012
Stephine Rasberry <srasberry00@gmail.com>
2022-07-04 11:45:46 -0400

    changed scroll to top button

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -1,108 +1,74 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
+// wait for the DOM content to be fully loaded before executing javaScript code
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
- */
+window.addEventListener("DOMContentLoaded", () => {
+  // Global Variables
+  const sections = document.getElementsByTagName("section");
+  const navList = document.getElementById("navbar__list");
+  const myButton = document.getElementById("myBtn");
 
-// build the nav
-const menu = document.getElementById("navbar-list");
-const sections = [...document.querySelectorAll(".section")];
-
-const nav_menu_items = () => {
-  let nav_container = "";
-
-  sections.forEach((section) => {
-    const sectionID = section.id;
-    const sectionAtrribute = section.dataset.nav;
-    nav_container += `<li> <a class="navbar-menu" href="#${sectionID}">${sectionAtrribute}</a></li>`;
-  });
-  menu.innerHTML = nav_container;
-};
-nav_menu_items();
-
-// create active btn on nav list
-const a = document.querySelectorAll(".navbar-menu a");
-for (let i = 0, length = a.length; i < length; i++) {
-  a[i].onclick = function () {
-    let b = document.querySelector(".navbar-menu .activeBtn");
-    if (b) b.classList.remove("activeBtn");
-    this.parentNode.classList.add("activeBtn");
+  // ********** Back to top button ***********
+  // Select the button
+  window.onscroll = function () {
+    scrollFunction();
   };
-}
-
-// Add class 'active' to section when near top of viewport
-const viewportValue = (section) => {
-  return Math.floor(section.getBoundingClientRect().top);
-};
-// Remove active class from previous section
-const removeClass = (section) => {
-  section.classList.remove("active");
-};
-
-// Add active class to section in view
-const addClass = (inView, section) => {
-  if (inView) {
-    section.classList.add("active");
+// ********** Add scroll funtion **********
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      myButton.style.display = "block";
+    } else {
+      myButton.style.display = "none";
+    }
   }
-};
-
-const activate = () => {
-  sections.forEach((section) => {
-    const element = viewportValue(section);
-    inviewport = () => element <= 300 && element >= 300;
-  });
-  removeClass(section);
-  addClass(inviewport().section);
-};
-
-// Scroll to anchor ID using scrollTO event
-var topBtn = document.getElementById("topBtn");
-// Show button when user scrolls down 50px
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    topBtn.style.display = "block";
-  } else {
-    topBtn.style.display = "none";
+  // ****** Scroll to top when button is clicked ******
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
   }
-}
 
-// Scroll to top when button clicked
-function topFunction() {
-  //
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
+  // ********* Adding the Nav bar sections **********
+  for (const section of sections) {
+    const menuItems = document.createElement("li");
+    const navLinks = document.createElement("a");
+    navLinks.textContent = section.dataset.nav;
+    navLinks.classList.add("menu__link");
+    navLinks.href = `#${section.id}`;
+    menuItems.append(navLinks);
+    navList.appendChild(menuItems);
+
+    // ********** Add smooth scroll behavior **********
+    navLinks.addEventListener("click", (e) => {
+      e.preventDefault();
+      const activeSection = document.getElementById(
+        e.target.getAttribute("href").substring(1)
+      );
+      activeSection.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    });
+  }
+
+  // ***** This section control all the scrolling behaviour *****
+  window.addEventListener("scroll", () => {
+    for (const section of sections) {
+      const activeSection = document.querySelectorAll(`#${section.id} h2`);
+      for (h2 of activeSection) {
+        const activeNav = document.querySelector(`[href="#${section.id}"]`);
+        if (section.className === "your-active-class") {
+          section.classList.remove("your-active-class");
+          activeNav.classList.remove("navigation");
+        }
+        if (
+          section.getBoundingClientRect().top >= 0 &&
+          section.getBoundingClientRect().top < 300
+        ) {
+          section.classList.add("your-active-class");
+          activeNav.classList.add("navigation");
+        }
+      }
+    }
   });
-}
-
-//
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
+});
